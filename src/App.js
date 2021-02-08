a5da914aecc9b10d52b6bea2e16cf4dfad389278
hbalty <balty.houssem@gmail.com>
2021-02-08 17:57:49 +0100

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
-import { Navbar, NavbarBrand} from 'reactstrap'
+import React, { Component } from 'react';
+import { Card, CardImg, CardTitle, CardText, CardBody } from 'reactstrap';
 import Menu from './components/MenuComponent.component';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App"> 
-      <Navbar dark color="primary">
-        <div className="container"> 
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu></Menu>
-    </div>
-  );
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
+
+  render() {
+    return ( 
+      <div className="container"> 
+          <div className="row">
+              <Menu dishes={DISHES}/>
+          </div>
+      </div>
+  ); 
+  }
+
+
+
 }
+
+
+
 
 export default App;
